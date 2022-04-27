b71392dd575be03cd26310f96c805bf6b965532e
ahmetterzi <ahmetterzi77@gmail.com>
2022-04-27 13:41:28 +0300

    Ahmet Terzi 27/04/2022

--- src/minNumber.java
+++ src/minNumber.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class minNumber {
 
       /*
@@ -20,7 +22,19 @@ public class minNumber {
      */
 
 
+    public static void main(String[] args) {
 
+        int arr[][] = {{1,2,3}, {2,3,1} , {5,5,5} , {2,1,3}};
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < arr.length; i++) {
+            Arrays.sort(arr[i]);
+
+            if(arr[i][0] < min) min = arr[i][0];
+        }
+        System.out.println("Minimum = " + min);
+    }
 
 
 
